32180eab24d2cd921a3091672d8869251a40c39c
Ayush Roy <94052139+AyushRoy2001@users.noreply.github.com>
2023-07-31 11:33:40 +0530

    Update distributions.py

--- distributions.py
+++ distributions.py
@@ -15,14 +15,14 @@ class Distribution:
     def __init__(self, projected_tg, projected_tb):
         # initialising from random uniform distributions
         self._mu_g = nn.Parameter(torch.mean(projected_tg, dim=0, keepdim=True), requires_grad=True).to(args.device)
-        self._A_g = nn.Parameter(torch.eye(args.random_samples, device=args.device), requires_grad=True)
+        self._A_g = nn.Parameter(torch.eye(self._mu_g.size(1), device=args.device)*0.5, requires_grad=True)
         self._mu_b = nn.Parameter(torch.mean(projected_tb, dim=0, keepdim=True), requires_grad=True).to(args.device)
-        self._A_b = nn.Parameter(torch.eye(args.random_samples, device=args.device), requires_grad=True)
+        self._A_b = nn.Parameter(torch.eye(self._mu_g.size(1), device=args.device)*0.5, requires_grad=True)
     
     def generateSamples(self, good=True): 
-        z = torch.randn(args.random_samples, args.random_samples, device=args.device, requires_grad=True)
+        z = torch.randn(args.random_samples, self._mu_g.size(1), device=args.device, requires_grad=True)
         if good:
-            t = self._mu_g + torch.matmul(self._A_g, z)
+            t = self._mu_g + torch.matmul(z, self._A_g.T)
         else:
-            t = self._mu_b + torch.matmul(self._A_b, z)
+            t = self._mu_b + torch.matmul(z, self._A_b.T)
         return t
